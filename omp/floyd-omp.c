f2b3a19bb7cdb846e044c5d866a0630c149c7970
Tan Yan <tanyan@ghc84.ghc.andrew.cmu.edu>
2021-04-24 22:57:53 -0400

    floyd omp

---
+++ omp/floyd-omp.c
@@ -0,0 +1,133 @@
+#include <assert.h>
+#include <error.h>
+#include <limits.h>
+#include <omp.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SYSEXPECT(expr) \
+  do {                  \
+    if (!(expr)) {      \
+      perror(__func__); \
+      exit(1);          \
+    }                   \
+  } while (0)
+#define error_exit(fmt, ...)                                    \
+  do {                                                          \
+    fprintf(stderr, "%s error: " fmt, __func__, ##__VA_ARGS__); \
+    exit(1);                                                    \
+  } while (0);
+
+int N = -1;     // number of vertices in the file.
+int* G = NULL;  // adjacency matrix representation of the graph
+int* D = NULL;  // matrix of distances
+int NCORES = -1;
+
+#define RC(i, j) (i * N + j)
+#define INF (N * N * 100)
+
+// set G[i,j] to value
+inline static void set_G(int i, int j, int value) {
+  assert(value >= 0 || value == -1);
+  G[RC(i, j)] = (value >= 0) ? value : INF;
+  return;
+}
+
+// returns value at G[i,j]
+inline static int get_G(int i, int j) {
+  return G[RC(i, j)];
+}
+
+void init_G(FILE* fp) {
+  // Allocate memory and read the matrix
+  G = (int*)malloc(N * N * sizeof(int));
+  SYSEXPECT(G != NULL);
+  int scan_ret;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int t;
+      scan_ret = fscanf(fp, "%d", &t);
+      if (scan_ret != 1)
+        error_exit("Failed to read G(%d, %d)\n", i, j);
+      set_G(i, j, t);
+    }
+  }
+  fclose(fp);
+}
+
+// prints results
+void apsp_print_result() {
+  int sum1 = 0;
+  int sum2 = 0;
+  printf("========== Solution ==========\n");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int d = D[RC(i, j)];
+      if (d >= INF) {
+        d = -1;
+      }
+      sum1 = (sum1 + d) % 255;
+      sum2 = (sum2 + sum1) % 255;
+      printf("%d ", d);
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+  printf("Checksum: %d", (sum2 << 8) | sum1);
+  putchar('\n');
+  return;
+}
+
+void apsp_start() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      D[RC(i, j)] = get_G(i, j);
+    }
+  }
+  for (int k = 0; k < N; k++) {
+#pragma omp parallel for num_threads(NCORES)
+    for (int ind = 0; ind < N * N; ind++) {
+      int i = ind / N;
+      int j = ind % N;
+      int d = D[RC(i, k)] + D[RC(k, j)];
+      if (d < D[RC(i, j)]) {
+        D[RC(i, j)] = d;
+      }
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  if (argc < 4 || strcmp(argv[1], "-p") != 0)
+    error_exit("Expecting two arguments: -p [processor count] and [file name]\n");
+  NCORES = atoi(argv[2]);
+  if (NCORES < 1)
+    error_exit("Illegal core count: %d\n", NCORES);
+  char* filename = argv[3];
+  FILE* fp = fopen(filename, "r");
+  if (fp == NULL)
+    error_exit("Failed to open input file \"%s\"\n", filename);
+  int scan_ret;
+  scan_ret = fscanf(fp, "%d", &N);
+  if (scan_ret != 1)
+    error_exit("Failed to read vertex count\n");
+  if (N < 2) {
+    error_exit("Illegal vertex count: %d\n", N);
+  }
+  init_G(fp);
+  D = (int*)malloc(N * N * sizeof(int));
+  SYSEXPECT(D != NULL);
+  struct timespec before, after;
+  clock_gettime(CLOCK_REALTIME, &before);
+  apsp_start();
+  clock_gettime(CLOCK_REALTIME, &after);
+  double delta_ms = (double)(after.tv_sec - before.tv_sec) * 1000.0 + (after.tv_nsec - before.tv_nsec) / 1000000.0;
+  putchar('\n');
+  printf("============ Time ============\n");
+  printf("Time: %.3f ms (%.3f s)\n", delta_ms, delta_ms / 1000.0);
+  apsp_print_result();
+  return 0;
+}
