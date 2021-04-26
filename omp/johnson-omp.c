07cd49d7ca3410c0a4a97f862a9a0fe96e06dec4
Catherine Lee <csl2@andrew.cmu.edu>
2021-04-25 20:03:20 -0400

    adjacency list for johnson

--- omp/johnson-omp.c
+++ omp/johnson-omp.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bits/stdc++.h>
+#include <boost/heap/fibonacci_heap.hpp>
 
 #define SYSEXPECT(expr) \
   do {                  \
@@ -82,6 +83,8 @@ void apsp_print_result() {
   return;
 }
 
+int* modified_G = NULL;
+
 void dijkstra(int s) {
   int* dist = (int*)malloc(sizeof(int) * N);
   for (int i = 0; i < N; i++) {
@@ -96,11 +99,12 @@ void dijkstra(int s) {
   while(!pq.empty()) {
     int u = pq.top().second;
     pq.pop();
-    for (int v = 0; v < N; v++) {
-      int weight = get_G(u, v);
-      if (weight != -1 && weight < INF && dist[u] + weight < dist[v]) {
-        dist[v] = dist[u] + weight;
-        pq.push(std::make_pair(-dist[v], v));
+    for (int v = 0; v < N && modified_G[RC(u,v)] != -1; v++) {
+      int real_v = modified_G[RC(u,v)];
+      int weight = get_G(u, real_v);
+      if (dist[u] + weight < dist[real_v]) {
+        dist[real_v] = dist[u] + weight;
+        pq.push(std::make_pair(-dist[real_v], real_v));
       }
     }
   }
@@ -109,7 +113,26 @@ void dijkstra(int s) {
   }
 }
 
+
 void apsp_start() {
+
+  modified_G = (int*) malloc(N*N*sizeof(int));
+
+// THIS ONE SOMEHOW MAKES IT SLOWER
+// #pragma omp parallel for num_threads(NCORES) 
+  for (int i = 0; i < N; i++) {
+    int counter = 0;
+    for (int j = 0; j < N; j++) {
+      if (get_G(i, j) < INF) {
+        modified_G[RC(i, counter)] = j;
+        counter++;
+      }
+    }
+    if (counter < N) {
+      modified_G[RC(i, counter)] = -1;
+    }
+  }
+
   int* bellman_ford = (int*) malloc((N + 1) * sizeof(int));
 
   bellman_ford[N] = 0;
@@ -120,24 +143,33 @@ void apsp_start() {
   for (int i = 0; i < N + 1; i++) {
 #pragma omp parallel for num_threads(NCORES)
     for (int u = 0; u < N + 1; u++) {
-      for (int v = 0; v < N + 1; v++) {
-        int weight;
-        if (u == N || v == N) {
-          weight = 0;
-        } else {
-          weight = get_G(u, v);
+      if (u == N) {
+        for (int v = 0; v < N; v++) {
+          if (bellman_ford[u] < bellman_ford[v]) {
+            bellman_ford[v] = bellman_ford[u];
+          }
         }
-        if (weight != -1 && weight < INF && bellman_ford[u] + weight < bellman_ford[v]) {
-          bellman_ford[v] = bellman_ford[u] + weight;
+      } else {
+        for (int v = 0; v < N && modified_G[RC(u, v)] != -1; v++) {
+          int real_v = modified_G[RC(u, v)];
+          int weight = get_G(u, real_v);
+          if (bellman_ford[u] + weight < bellman_ford[real_v]) {
+            bellman_ford[real_v] = bellman_ford[u] + weight;
+          }
+        }
+        if (bellman_ford[u] < bellman_ford[N]) {
+          bellman_ford[N] = bellman_ford[u];
         }
       }
     }
   }
+  
 
 #pragma omp parallel for num_threads(NCORES)
   for (int u = 0; u < N; u++) {
-    for (int v = 0; v < N; v++) {
-      G[RC(u, v)] = get_G(u, v) + bellman_ford[u] - bellman_ford[v];
+    for (int v = 0; v < N && modified_G[RC(u, v)] != -1; v++) {
+      int real_v = modified_G[RC(u, v)];
+      G[RC(u, real_v)] = get_G(u, real_v) + bellman_ford[u] - bellman_ford[real_v];
     }
   }
 
